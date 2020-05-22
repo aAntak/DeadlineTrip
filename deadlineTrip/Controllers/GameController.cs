615a8bdb9b9caf21ea53723ecd5b1284132a86c3
IgnasBarakauskas <b.ignas05@gmail.com>
2020-05-22 19:23:16 +0300

    I am pro of MVC

---
+++ deadlineTrip/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using deadlineTrip.Models;
+using deadlineTrip.Models.APIs;
+using deadlineTrip.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deadlineTrip.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IAdvertisementRepository _advertisementRepository;
+        public GameController(IAdvertisementRepository advertisementRepository,
+            IGameRepository gameRepository)
+        {
+            _advertisementRepository = advertisementRepository;
+            _gameRepository = gameRepository;
+        }
+        public ViewResult list() 
+        {
+            return View("List");
+        }
+    }
+}
